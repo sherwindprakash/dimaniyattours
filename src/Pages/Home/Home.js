4df7eec83b85c28557a6f43135bac0864166374f
SherwinDPrakash <sherwindprakash@ymail.com>
2023-03-21 21:13:39 +0400

    1

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./Home.css";
 import Home_Slider from "../../components/Home_Slider/Home_Slider";
+import Home_Packages from "../../components/Package/Package";
 
 export default function Home() {
   useEffect(() => {
--- src/components/Package/Package.js
+++ src/components/Package/Package.js
@@ -24,7 +24,7 @@ function PackageSingle({ ID }) {
         });
     };
     fetchData();
-  }, []);
+  }, [ID]);
 
   if (!post) return null;
 
