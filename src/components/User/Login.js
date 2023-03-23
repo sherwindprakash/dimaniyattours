c1e94c9b1d1d0c64b646df84e40501b27b4846ee
Sherwin Prakas <sherwin@tbwazeenah.local>
2023-03-23 16:59:01 +0400

    123

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Login() {
+const HomeSignUp = () => {
+  const [FullName, setFullName] = useState("");
+
+  const handleSubmit = (event) => {
+    // 👇️ prevent page refresh
+    event.preventDefault();
+    // DATA
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      user: {
+        user: "prakash@sherwin.com",
+        password: "prakash",
+        email: "prakash@sherwin.com",
+        group: "author",
+        Full_Name: FullName,
+        Nationality: "Male",
+        DayPhone: "96237694",
+        active: true,
+        api_key: true,
+      },
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      "http://localhost/dimaniyattours_api/admin/admin/api/cockpit/saveUser?token=22f8709abba293936facc262597237",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+    //
+  };
+
   return (
     <>
-      <h2>LOGIN</h2>
+      <form id="SignUPFrom" className="SignUPForm" onSubmit={handleSubmit}>
+        <input
+          className="form-input"
+          type="text"
+          id="Emergency_Contact_Name"
+          name="Emergency_Contact_Name"
+          value={FullName}
+          onChange={(event) => setFullName(event.target.value)}
+          //   required
+          placeholder=""
+        />
+        <button className="BUTSignUP" type="submit">
+          ADD TO CART
+        </button>
+      </form>
     </>
   );
-}
+};
+
+export default HomeSignUp;
