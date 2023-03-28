59e858a9676300a314a9f343b26f5e0d1c32a207
Sherwin Prakas <sherwin@tbwazeenah.local>
2023-03-28 15:24:48 +0400

    up

--- src/components/Home_Slider/Home_Slider.js
+++ src/components/Home_Slider/Home_Slider.js
@@ -9,9 +9,10 @@ import "swiper/css/scrollbar";
 // import required modules
 import { Scrollbar } from "swiper";
 // LOAD IMG
-import IMG from "./dummy_480x700_ffffff_cccccc.png"
-import NEXT from "../../assets/right-arrow-next-svgrepo-com.svg"
+import IMG from "./dummy_480x700_ffffff_cccccc.png";
+import Destination from "./Destination.mp4";
 
+import NEXT from "../../assets/right-arrow-next-svgrepo-com.svg";
 
 export default function App() {
   return (
@@ -19,7 +20,6 @@ export default function App() {
       <Swiper
         slidesPerView={1}
         spaceBetween={0}
-
         breakpoints={{
           640: {
             slidesPerView: 1,
@@ -45,72 +45,129 @@ export default function App() {
         modules={[Scrollbar]}
         className="mySwiper"
       >
-
-          <SwiperSlide>
+        <SwiperSlide>
           <Link title="Destination" to="/en/destination/">
             <div className="contHolder">
               <h2>All year long travel</h2>
-              <h3>Let's explore all
-                Wondderful
-                Dimaniyat
-                Destinations</h3>
+              <h3>Let's explore all Wondderful Dimaniyat Destinations</h3>
             </div>
-            <img fetchpriority="low" height="700" width="480" alt="Destination" title="Destination" src={IMG} />
-            </Link>
 
-          </SwiperSlide>
+            <video
+              className="lazy"
+              autoPlay
+              muted
+              loop
+              playsInline
+              width="100%"
+              height="100%"
+              // poster="one-does-not-simply.jpg"
+              src={Destination}
+              type="video/mp4"
+            ></video>
 
-
-        <SwiperSlide>
-        <Link title="Shorts Trips" to="/en/shorts-trips/">
-        <div className="contHolder" style={{
-            transform: "rotate(180deg)"
-          }}>
-          <h3 style={{
-            transform: "rotate(180deg)",
-            marginTop: "20px"
-          }}>Shorts Trips</h3>
-          
-          </div>
-          <img fetchpriority="low" height="700" width="480" alt="Gallery" title="Shorts Trips" src={IMG} />
+            {/* <img
+              fetchpriority="low"
+              height="700"
+              width="480"
+              alt="Destination"
+              title="Destination"
+              src={IMG}
+            /> */}
           </Link>
         </SwiperSlide>
 
         <SwiperSlide>
-        <Link title="Special Offers" to="/en/special-offers/">
-          <div className="contHolder" style={{
-            transform: "rotate(180deg)"
-          }}>
-          <h3 style={{
-            transform: "rotate(180deg)",
-            marginTop: "20px"
-          }}>Special Offers</h3>
-          <h2 style={{
-            transform: "rotate(180deg)"
-          }}>Travel</h2>
-          </div>
-          <img className="Next" alt="Next" title="Next" src={NEXT} />
-          <img fetchpriority="low" height="700" width="480" alt="Special Offers" title="Special Offers" src={IMG} />
+          <Link title="Shorts Trips" to="/en/shorts-trips/">
+            <div
+              className="contHolder"
+              style={{
+                transform: "rotate(180deg)",
+              }}
+            >
+              <h3
+                style={{
+                  transform: "rotate(180deg)",
+                  marginTop: "20px",
+                }}
+              >
+                Shorts Trips
+              </h3>
+            </div>
+            <img
+              fetchpriority="low"
+              height="700"
+              width="480"
+              alt="Gallery"
+              title="Shorts Trips"
+              src={IMG}
+            />
           </Link>
         </SwiperSlide>
 
         <SwiperSlide>
-        <Link title="Gallery" to="/en/marine-service/">
-        <div className="contHolder" style={{
-            transform: "rotate(180deg)"
-          }}>
-          <h3 style={{
-            transform: "rotate(180deg)",
-            marginTop: "20px"
-          }}>Marine Service</h3>
-          
-          </div>
-          <img fetchpriority="low" height="700" width="480" alt="Marine Service" title="Marine Service" src={IMG} />
+          <Link title="Special Offers" to="/en/special-offers/">
+            <div
+              className="contHolder"
+              style={{
+                transform: "rotate(180deg)",
+              }}
+            >
+              <h3
+                style={{
+                  transform: "rotate(180deg)",
+                  marginTop: "20px",
+                }}
+              >
+                Special Offers
+              </h3>
+              <h2
+                style={{
+                  transform: "rotate(180deg)",
+                }}
+              >
+                Travel
+              </h2>
+            </div>
+            <img className="Next" alt="Next" title="Next" src={NEXT} />
+            <img
+              fetchpriority="low"
+              height="700"
+              width="480"
+              alt="Special Offers"
+              title="Special Offers"
+              src={IMG}
+            />
           </Link>
         </SwiperSlide>
 
+        <SwiperSlide>
+          <Link title="Gallery" to="/en/marine-service/">
+            <div
+              className="contHolder"
+              style={{
+                transform: "rotate(180deg)",
+              }}
+            >
+              <h3
+                style={{
+                  transform: "rotate(180deg)",
+                  marginTop: "20px",
+                }}
+              >
+                Marine Service
+              </h3>
+            </div>
+            <img
+              fetchpriority="low"
+              height="700"
+              width="480"
+              alt="Marine Service"
+              title="Marine Service"
+              src={IMG}
+            />
+          </Link>
+        </SwiperSlide>
       </Swiper>
     </>
   );
 }
-
